575ab994d040d9c42c72913fb91719d13fd33050
FedorPeplinChamp <fedor.peplin@smcontact.ru>
2020-04-09 14:48:25 +0300

    Task3 refreshed

--- Task3.py
+++ Task3.py
@@ -1,16 +1,15 @@
 documents = [
- {"type": "passport", "number": "2207 876234"},
- {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
- {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+ {"type": "passport", "number": "2207 876234", "name": "Геннадий Покемонов"},
+ {"type": "invoice", "number": "11-2", "name": "Вася Пупкин"},
+ {"type": "insurance", "number": "10006",}
  ]
 
 def exception():
         for i in documents:
             try:
-                print (i ["name"])
+                namecheck=i["name"]
             except KeyError:
-                print ('У одного из владельцев документа отсутствует имя')
-                break
-
+                indexoferror=int(documents.index(i))+1 #прибавляем 1, т.к. нумерация "людей" начинается с 1, а не 0, как делает питон
+                print ('У одного из владельцев документа отсутствует имя, а именно у пользователя номер', indexoferror)
 
 exception()
